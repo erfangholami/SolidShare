17af1fb16b86495f821574ed45325a303ec0fd39
Erfan Gholami <erfangholami76@gmail.com>
2025-10-28 17:08:32 +0100

    Basic UI

--- composeApp/src/commonMain/kotlin/com/erfangh/solidshare/App.kt
+++ composeApp/src/commonMain/kotlin/com/erfangh/solidshare/App.kt
@@ -3,6 +3,7 @@ package com.erfangh.solidshare
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,27 +24,17 @@ import solidshare.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
+        Box(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            Text(
+                text =  "Solid Share",
+                modifier = Modifier
+                    .align(Alignment.Center)
+            )
         }
     }
 }
